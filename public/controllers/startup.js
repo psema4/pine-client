fea7bbc3aaf7cd0598ff2a16c0c287bcf7f48d36
Scott Elcomb <psema4@gmail.com>
2013-07-09 14:25:25 -0400

    add placeholder pine logo

--- public/controllers/startup.js
+++ public/controllers/startup.js
@@ -3,7 +3,7 @@ var testApp = angular.module('testapp', []);
 function startupController($scope, $location, splash) {
     $scope.splash = splash;
 
-    splash({ deck: ['splash1.png', 'splash2.png'], prefix: 'assets/splash/', delay: 3000 });
+    splash({ deck: ['pine.png', 'splash2.png'], prefix: 'assets/splash/', delay: 3000 });
 }
 
 testApp.controller('startupController', startupController);
