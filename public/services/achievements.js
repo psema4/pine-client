618a460ea0f73badceb7fb6556f6544281460ef3
Alex Wilson <arexkun11@gmail.com>
2013-07-12 19:55:09 -0400

    use toast to notify user about achievements

--- public/services/achievements.js
+++ public/services/achievements.js
@@ -1,6 +1,17 @@
 var services = angular.module('testapp.services');
 
-services.factory('Achievements', ['$http', '$routeParams', function($http, $routeParams) {
+services.factory('Achievements', ['$http', '$routeParams', 'Toast', function($http, $routeParams, toast) {
+    function notify (slug) {
+        $http({ method: 'GET', params: {game: $routeParams.id, slug: slug}, url: '/achievements/about' }).
+            success(function(data, status, headers, config) {
+                toast({ msg: '<b>Unlocked: ' + data.title + '</b><br>' + data.description });
+            }).
+            error(function(data, status, headers, config) {
+                if (cb && typeof cb == 'function') cb(status);
+            })
+        ;
+    }
+
     return {
         incr: function (slug, amount, options, cb) {
             if (typeof amount === 'object' || amount == null) {
@@ -15,7 +26,7 @@ services.factory('Achievements', ['$http', '$routeParams', function($http, $rout
 
             $http({ method: 'GET', params: {game: $routeParams.id, slug: slug, amount: amount}, url: '/achievements/incr' }).
                 success(function(data, status, headers, config) {
-                    if (data === 'true' && !(options && options.silent === true)) console.log("Unlocked achievement '" + slug + "'");
+                    if (data === 'true' && !(options && options.silent === true)) notify(slug);
                     if (cb && typeof cb == 'function') cb(status);
                 }).
                 error(function(data, status, headers, config) {
@@ -37,7 +48,7 @@ services.factory('Achievements', ['$http', '$routeParams', function($http, $rout
 
             $http({ method: 'GET', params: {game: $routeParams.id, slug: slug, amount: amount}, url: '/achievements/set' }).
                 success(function(data, status, headers, config) {
-                    if (data === 'true' && !(options && options.silent === true)) console.log("Unlocked achievement '" + slug + "'");
+                    if (data === 'true' && !(options && options.silent === true)) notify(slug);
                     if (cb && typeof cb == 'function') cb(status);
                 }).
                 error(function(data, status, headers, config) {
@@ -67,7 +78,7 @@ services.factory('Achievements', ['$http', '$routeParams', function($http, $rout
 
             $http({ method: 'GET', params: {game: $routeParams.id, slug: slug}, url: '/achievements/unlock' }).
                 success(function(data, status, headers, config) {
-                    if (data === 'true' && !(options && options.silent === true)) console.log("Unlocked achievement '" + slug + "'");
+                    if (data === 'true' && !(options && options.silent === true)) notify(slug);
                     if (cb && typeof cb == 'function') {
                         cb(data);
                     }
